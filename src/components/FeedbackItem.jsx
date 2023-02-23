642e2cb8888807543257eec97ff225dcd04eefa7
ozan isgor <ozanisgor11@gmail.com>
2023-02-23 20:11:30 +0300

    update: Delete & Update button appearance

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -3,7 +3,6 @@ import { useContext } from "react"
 import PropTypes from "prop-types"
 import Card from "./shared/Card"
 import FeedbackContext from "../context/FeedbackContext"
-import { toast } from "react-toastify"
 import { getAuth } from "firebase/auth"
 
 function FeedbackItem({ item }) {
@@ -14,27 +13,28 @@ function FeedbackItem({ item }) {
   const handleDelete = () => {
     if (auth.currentUser && item.userRef === auth.currentUser.uid) {
       deleteFeedback(item.id)
-    } else {
-      toast.error("You need to be owner of the feedback")
     }
   }
+
   const handleUpdate = () => {
     if (auth.currentUser && item.userRef === auth.currentUser.uid) {
       editFeedback(item)
-    } else {
-      toast.error("You need to be owner of the feedback")
     }
   }
 
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
-      <button onClick={handleDelete} className='close'>
-        <FaTimes color='purple' />
-      </button>
-      <button onClick={handleUpdate} className='edit'>
-        <FaEdit color='purple' />
-      </button>
+      {auth.currentUser && item.userRef === auth.currentUser.uid && (
+        <button onClick={handleDelete} className='close'>
+          <FaTimes color='purple' />
+        </button>
+      )}
+      {auth.currentUser && item.userRef === auth.currentUser.uid && (
+        <button onClick={handleUpdate} className='edit'>
+          <FaEdit color='purple' />
+        </button>
+      )}
       <div className='text-display'>{item.text}</div>
     </Card>
   )
